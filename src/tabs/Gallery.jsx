7f81fba50073d38227ad6733ec6403025e747589
Alla <urt2509@gmail.com>
2023-06-24 15:29:30 +0200

    task1 done

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -9,6 +9,7 @@ export class Gallery extends Component {
     page: 1,
     images: [],
     total_images: 0,
+    error: null,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -24,20 +25,53 @@ export class Gallery extends Component {
         seacrh,
         page
       );
+
+      this.setState(prevValue => ({
+        images: [...prevValue.images, ...photos],
+        total_images: total_results,
+      }));
     } catch (error) {
+      this.setState({ error: error.name });
       console.log(error);
     }
   };
 
   onFormSubmit = evt => {
-    this.setState({ seacrh: evt });
+    this.setState({ seacrh: evt, page: 1, images: [] });
+  };
+
+  onButtonClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
+    const { images, total_images, seacrh, error } = this.state;
+    console.log(error);
     return (
       <>
         <SearchForm onSubmit={this.onFormSubmit} />
-        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
+        {images.length === 0 && seacrh !== '' && !error && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+
+        {error && <Text textAlign="center">{error}</Text>}
+
+        <Grid>
+          {images.map(({ id, avg_color, alt, src: { large } }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
+        {images.length > 0 && images.length !== total_images && (
+          <Button type="button" onClick={this.onButtonClick}>
+            Load more
+          </Button>
+        )}
       </>
     );
   }
