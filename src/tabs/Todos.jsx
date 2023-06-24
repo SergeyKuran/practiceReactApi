488faa2473bc7483a3b0faa5a934bdd6983ea5e3
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-06-24 17:16:43 +0300

    Add features for Todos

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,51 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  onFormSubmit = text => {
+    const todo = { id: nanoid(), text };
+    this.setState(({ todos }) => ({ todos: [...todos, todo] }));
+  };
+
+  handleDeleteTodo = todoId => {
+    this.setState(({ todos }) => ({
+      todos: todos.filter(({ id }) => id !== todoId),
+    }));
+  };
+
+  // mapTodos = ({ id, text }, idx) => (
+  //   <GridItem key={id}>
+  //     <Todo
+  //       id={id}
+  //       text={text}
+  //       idx={idx}
+  //       handleDeleteTodo={this.handleDeleteTodo}
+  //     />
+  //   </GridItem>
+  // );
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <>
+        <SearchForm onSubmit={this.onFormSubmit} />
+
+        <Grid>
+          {todos.map(({ id, text }, idx) => (
+            <GridItem key={id}>
+              <Todo
+                id={id}
+                text={text}
+                idx={idx}
+                handleDeleteTodo={this.handleDeleteTodo}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
+    );
   }
 }
